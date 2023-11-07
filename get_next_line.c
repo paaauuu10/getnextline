9e4777785c78effbdfdd377c69389233ffb18b64
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2023-11-07 14:21:55 +0100

    next step

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,32 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:17:45 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/10/31 11:17:46 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/07 14:20:14 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char    *ft_free_storage(char **storage)
+{
+    free(*storage);
+    *storage = NULL;
+    return (NULL);
+}
+
+
+    
+char    *get_next_line(int fd)
+{
+    static char *storage;
+    char        *line;
+    
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    storage = ft_read(fd, storage);
+    if(!storage)
+        return (NULL);
+    line = ft_getlines(storage, 0);
+    if (!line)
+        return (ft_free_storage(&storage));
+    storage = ft_clean_storage(storage);
+    return (line);        
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,25 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:16:10 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/10/31 11:16:13 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/07 14:20:53 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+char    *get_next_line(int fd);
+char    *ft_read(int fd, char *storage);
+char    *ft_getlines(char *storage, int i);
+char    *ft_free_storage(char **storage);
+char    *ft_clean_storage(char *storage); 
+
+#endif
