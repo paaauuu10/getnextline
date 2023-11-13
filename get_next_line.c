9cd2962160b39f2fe6809edd277c475074c97e4c
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2023-11-13 11:33:51 +0100

    una mica mes

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,11 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:17:45 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/11/07 14:20:14 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/13 11:33:23 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
 char    *ft_free_storage(char **storage)
 {
@@ -17,8 +19,53 @@ char    *ft_free_storage(char **storage)
     return (NULL);
 }
 
+char    *ft_get_lines(char *storage, int i)
+{
+    char    *memory;
+    if (!storage[i])
+        return (NULL);
+    while (storage[i] && storage[i] != '\n');
+        i++;
+    if (storage[i] == '\n')
+        i++;
+    memory = (char *)malloc((i + 1) * sizeof(char));
+    if (!memory)
+        return (NULL);
+    i = 0;
+    while (storage[i] && storage[i] != '\n')
+    {
+        memory[i] = storage [i];
+        i++;
+    }
+    memory[i] = '\0';
+    return (memory);
+}
+char    *ft_read(int fd, char *storage)
+{
+    char    *memory;
+    int     bytes;
+
+    memory = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if (!memory)
+        return (ft_free_storage(&storage));
+    bytes = 1;
+    while (!ft_strchr(storage, '\n') && bytes != 0)
+    {
+        bytes = read(fd, memory, BUFFER_SIZE);
+        if (bytes == -1)
+        {
+            ft_free_storage(&storage);
+            ft_free_storage(&memory);
+            return (NULL);
+        }
+        memory[bytes] = '\0';
+        storage = ft_strjoin(&storage, memory);
+    }
+    ft_free_storage(&memory);
+    return (storage);
 
     
+}   
 char    *get_next_line(int fd)
 {
     static char *storage;
@@ -29,7 +76,7 @@ char    *get_next_line(int fd)
     storage = ft_read(fd, storage);
     if(!storage)
         return (NULL);
-    line = ft_getlines(storage, 0);
+    line = ft_get_lines(storage, 0);
     if (!line)
         return (ft_free_storage(&storage));
     storage = ft_clean_storage(storage);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:16:10 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/11/07 14:20:53 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/13 10:57:15 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 
 char    *get_next_line(int fd);
 char    *ft_read(int fd, char *storage);
-char    *ft_getlines(char *storage, int i);
+char    *ft_get_lines(char *storage, int i);
 char    *ft_free_storage(char **storage);
 char    *ft_clean_storage(char *storage); 
 
