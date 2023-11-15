e984d9e85b01fa16ce49b89cbda5ce2f6a34ad32
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2023-11-15 15:45:48 +0100

    normi ok 100% paco

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:16:10 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/11/15 12:05:24 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/15 12:36:54 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,13 @@
 # include <stdlib.h>
 # include <limits.h>
 
-char    *get_next_line(int fd);
-char    *ft_read(int fd, char *storage);
-char    *ft_get_lines(char *storage, int i);
-char    *ft_free_storage(char **storage);
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *storage);
+char	*ft_get_lines(char *storage, int i);
+char	*ft_free_storage(char **storage);
 char	*ft_strchr(char *s, int c);
 char	*ft_strdup(char *s1);
 char	*ft_strjoin(char **s3, char *s2);
 size_t	ft_strlen(char *s);
-
-
-
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,62 +6,11 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:17:01 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/11/15 12:10:10 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/15 12:35:36 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-/*size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == (char)c)
-			return ((char *)(str + i));
-		i++;
-	}
-	if ((char)c == str[i])
-		return ((char *)(str + i));
-	return (NULL);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (!str)
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
-}*/
 
 size_t	ft_strlen(char *s)
 {
